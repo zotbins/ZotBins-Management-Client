9e1e9fc2d6ef4734477e2b91276111cc4435deb6
Jesse Chong <jec334@gmail.com>
2019-11-25 20:37:23 -0800

    Added sass compile script, refactored inline styling to sass file

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.scss';
+import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
