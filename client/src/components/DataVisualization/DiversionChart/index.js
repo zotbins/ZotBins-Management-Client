6dc88269c9b5e3f5afa8c353b6d67cbfbb0fbcb1
Bhavani Panda <sgpanda@uci.edu>
2020-10-27 21:42:22 -0700

    Removed landfill weight data
    - Now the diversion chart only plots the diversion rate.
    - Changed the font of the diversion chart text.

--- client/src/components/DataVisualization/DiversionChart/index.js
+++ client/src/components/DataVisualization/DiversionChart/index.js
@@ -6,64 +6,33 @@ class DiversionChart extends React.Component {
     
     componentDidMount() {
       const myChartRef = this.chartRef.current.getContext("2d");
-        
-      var tempChart = new Chart(myChartRef, {
+      
+      new Chart(myChartRef, {
           type: "line",
           options: {  
               legend: {
-                onClick: function(event, legnItem){
-                    var i = legnItem.datasetIndex;
-                    tempChart.data.datasets[i].hidden = !tempChart.data.datasets[i].hidden;
-                    tempChart.options.scales.yAxes[i].display = !tempChart.options.scales.yAxes[i].display;
-                    tempChart.update();
-                }
+                display: false
               },
               scales: {
                 xAxes: [{
-                    offset: false,
+                    offset: true,
                     scaleLabel: {
                         display: true,
-                        labelString: 'MONTH',
-                        fontSize: 15,
-                        fontFamily: 'georgia',
-                        fontStyle: 'bold'
+                        labelString: 'Month',
+                        fontSize: 20,
+                        fontFamily: "'Quicksand', sans-serif"
                     }
                 }],
                 yAxes: [
                 {
-                    id: 'A',
-                    type: 'linear',
-                    position: 'left',
-                    gridLines: {
-                        display: false,
-                    },
                     scaleLabel: {
                         display: true,
-                        labelString: 'LANDFILL WEIGHT (TONS)',
-                        fontSize: 14,
-                        fontFamily: 'georgia',
-                        fontStyle: 'bold'
+                        labelString: 'Diversion Rate (%)',
+                        fontSize: 20,
+                        fontFamily: "'Quicksand', sans-serif"
                     },
                     ticks: {
                         beginAtZero: true,
-                    }
-                },
-                {
-                    id: 'B',
-                    type: 'linear',
-                    position: 'right',
-                    gridLines: {
-                        display: false
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'DIVERSION RATE (%)',
-                        fontSize: 14,
-                        fontFamily: 'georgia',
-                        fontStyle: 'bold'
-                    },
-                    ticks: {
-                        beginAtZero: true
                     }
                 }]
               }
@@ -73,23 +42,12 @@ class DiversionChart extends React.Component {
               labels: ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"],
               datasets: [
                   {
-                      label: "Landfill Weight",
-                      data: [11056, 8340, 9991, 4304, 7600, 5300, 2567, 3999, 7940, 9110, 10580, 8345],
-                      yAxisID: 'A',
-                      borderColor: 'rgba(56, 118, 172, 0.53)',
-                      fill: false,
-                      pointStyle: 'rectRounded',
-                      borderWidth: 2,
-                      lineTension: 0      
-                  },
-                  {
                       label: "Diversion Rate",
                       data: [3, 47, 52, 34, 76, 53, 25, 85, 79, 91, 58, 83],
-                      yAxisID: 'B',
                       borderColor: 'rgba(53, 123, 35, 0.53)',
                       fill: false,
                       pointStyle: 'rectRounded',
-                      borderWidth: 2,
+                      borderWidth: 6,
                       lineTension: 0      
                   }
               ]
