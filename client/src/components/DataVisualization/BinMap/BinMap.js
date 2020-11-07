d973cb97a74120b33f7f353db0b3717ca637402e
jjchung3 <joejoonchung@gmail.com>
2020-11-07 02:15:12 -0800

    feat(binmap): adding markers...

--- client/src/components/DataVisualization/BinMap/BinMap.js
+++ client/src/components/DataVisualization/BinMap/BinMap.js
@@ -1,20 +1,43 @@
 import React from 'react'
 import { Card } from 'antd'
+import { bins } from './BinMap-mock'
 import Map from 'pigeon-maps'
+import Overlay from 'pigeon-overlay'
 
-class BinMap extends React.Component {
-  render() {
-    return (
-      <Card id="bin-map-card">
-        <Map
-          center={[33.6404952, -117.8442962]}
-          zoom={14}
-          height={250}
-          style={{ width: '100%' }}
-        ></Map>
-      </Card>
-    )
-  }
+const BinMap = () => {
+  return (
+    <Card id="bin-map-card">
+      <Map
+        center={[33.6404952, -117.8442962]}
+        zoom={15}
+        minZoom={14}
+        height={250}
+        style={{ width: '100%' }}
+      >
+        {bins.map((bin, index) => {
+          return (
+            <Overlay key={`overlay-${index}`} anchor={bin.location}>
+              <Marker weight={bin.weight} />
+            </Overlay>
+          )
+        })}
+      </Map>
+    </Card>
+  )
 }
+
+const Marker = React.memo(({ weight }) => {
+  return (
+    <div
+      style={{
+        background:
+          weight === -1 ? '#9b4538' : weight > 80 ? '#f9ba3c' : '#4b9f42',
+        borderRadius: '50%',
+        height: 12,
+        width: 12,
+      }}
+    />
+  )
+})
 
 export default BinMap
